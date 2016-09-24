bba96447268a26eba0dec7dbc6e5368f5aec7487
letsch <detlef.letsch@web.de>
2016-09-24 21:45:14 +0200

    different output formats

--- gpx/src/main/java/de/lis/fx/GpxCreatorUI.java
+++ gpx/src/main/java/de/lis/fx/GpxCreatorUI.java
@@ -6,10 +6,13 @@ package de.lis.fx;
 
 import de.lis.gpx.GpxCreator;
 import javafx.application.Application;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextInputDialog;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -21,22 +24,57 @@ public class GpxCreatorUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Grundstück Datei (.csv) auswählen.");
-        fileChooser.getExtensionFilters().addAll(
+/*
+        final ToggleGroup group = new ToggleGroup();
+        RadioButton rb1 = new RadioButton("GPX Route");
+        rb1.setToggleGroup(group);
+        rb1.setUserData(GpxCreator.OutputType.GPX_ROUTE);
+        rb1.setSelected(true);
+        RadioButton rb2 = new RadioButton("GPX Track");
+        rb2.setUserData(GpxCreator.OutputType.GPX_TRACK);
+        rb2.setToggleGroup(group);
+        RadioButton rb3 = new RadioButton("GPX Plain");
+        rb3.setToggleGroup(group);
+        rb3.setUserData(GpxCreator.OutputType.GPX_PLAIN);
+        RadioButton rb4 = new RadioButton("KML Polygon");
+        rb4.setUserData(GpxCreator.OutputType.KML_POLYGON);
+        rb4.setToggleGroup(group);
+*/
+        final GpxCreator.OutputType[] outputType = {GpxCreator.OutputType.GPX_ROUTE};
+        /*
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                outputType[0] = (GpxCreator.OutputType) newValue.getUserData();
+            }
+        });
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.getChildren().addAll(rb1, rb2, rb3);
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        System.out.println("Selected item:" + outputType[0].name());
+        */
+
+        FileChooser inFileChooser = new FileChooser();
+        inFileChooser.setTitle("Grundstück Datei (.csv) auswählen.");
+        inFileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("CSV Files", "*.csv"),
                 new FileChooser.ExtensionFilter("All Files", "*.*"));
-        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        File selectedFile = inFileChooser.showOpenDialog(primaryStage);
         if (selectedFile != null) {
             GpxCreator creator = new GpxCreator();
             String inputSource = selectedFile.getAbsolutePath();
             String outputSource = null;
 
-            fileChooser.setTitle("Ausgabedatei wählen oder anlegen (Endung: .gpx)" );
-            fileChooser.getExtensionFilters().setAll(new FileChooser.ExtensionFilter("GPX Files", "*.gpx"));
-            selectedFile = fileChooser.showSaveDialog(null);
+            FileChooser outFileChooser = new FileChooser();
+            outFileChooser.setTitle("Ausgabedatei wählen oder anlegen.  Endung:" + (outputType[0].equals(GpxCreator.OutputType.KML_POLYGON) ? ".kml" : "'.gpx'"));
+            outFileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("GPX Files", "*.gpx"),
+                    new FileChooser.ExtensionFilter("KML Files", "*.kml"));
+            selectedFile = outFileChooser.showSaveDialog(null);
             if (selectedFile != null) {
-
                 String header = System.getProperty("header");
                 if ( header == null || header.isEmpty() ) {
                     header = "Generierte GPX Koordinaten";
@@ -51,21 +89,14 @@ public class GpxCreatorUI extends Application {
                     System.setProperty("header", header);
                 }
                 outputSource = selectedFile.getAbsolutePath();
-                creator.createGpx(inputSource, outputSource);
+                creator.createGpx(inputSource, outputSource, outputType[0]);
             }
-
-            System.exit(0);
-
-
-            // System.out.println("File " + selectedFile + " selected");
-            // primaryStage.(selectedFile);
         }
+        System.exit(0);
     }
 
     public static void main(String[] args) {
         launch(args);
-
-
     }
 }
 
--- gpx/src/main/java/de/lis/gpx/GpxCreator.java
+++ gpx/src/main/java/de/lis/gpx/GpxCreator.java
@@ -6,11 +6,9 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.log.SystemLogChute;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.*;
-import java.net.URL;
 import java.util.*;
 
 /**
@@ -25,44 +23,44 @@ public class GpxCreator {
         Reader in = new FileReader(filename);
         Iterable<CSVRecord> records = CSVFormat.newFormat(';').withHeader().parse(in);
 
-        for (CSVRecord record:records) {
+        for (CSVRecord record : records) {
             Map wf = new HashMap();
             wf.put("gem", record.get("Gemarkung"));
             wf.put("fl", record.get("Flurkarte"));
             wf.put("fs", record.get("Flurstueck"));
             wf.put("desc", record.get("Bezeichnung"));
             String latlonStr = record.get("latlon");
-            if ( latlonStr.isEmpty() ) {
+            if (latlonStr.isEmpty()) {
                 String utm = record.get("utm32");
-                if ( utm.isEmpty() ) continue;
+                if (utm.isEmpty()) continue;
                 latlonStr = utm2latlon(utm);
             }
-            if ( latlonStr.isEmpty() ) continue;
+            if (latlonStr.isEmpty()) continue;
 
             List coords = new ArrayList();
             String latlons[] = latlonStr.split(", *");
-            for ( String latlon:latlons ) {
+            for (String latlon : latlons) {
                 Map ll = new HashMap();
-                if ( latlon.isEmpty() ) continue;
+                if (latlon.isEmpty()) continue;
                 String tmp[] = latlon.split(" ");
                 ll.put("lon", tmp[0]);
                 ll.put("lat", tmp[1]);
                 coords.add(ll);
             }
             int size = coords.size();
-            if ( size == 0 ) continue;
-            Map lastCoords = (Map) coords.get(size-1);
+            if (size == 0) continue;
+            Map lastCoords = (Map) coords.get(size - 1);
             Map firstCoords = (Map) coords.get(0);
-            if ( !(lastCoords.get("lat").equals(firstCoords.get("lat")) && lastCoords.get("lon").equals(firstCoords.get("lon"))) ) {
+            if (!(lastCoords.get("lat").equals(firstCoords.get("lat")) && lastCoords.get("lon").equals(firstCoords.get("lon")))) {
                 coords.add(firstCoords);
             }
 
-            wf.put("coords",coords);
+            wf.put("coords", coords);
             wfs.add(wf);
         }
     }
 
-    public List createGrundStuecke ( String inputSource ) throws IOException {
+    public List createGrundStuecke(String inputSource) throws IOException {
 
         readCsv(inputSource);
 
@@ -70,10 +68,10 @@ public class GpxCreator {
     }
 
     String utm2latlon(String utm) {
-        String latlon="";
+        String latlon = "";
         String utms[] = utm.split(", *");
         CoordinateConversion cc = new CoordinateConversion();
-        for ( String u:utms ) {
+        for (String u : utms) {
             String tmp = "32 N " + u;
             double ll[] = cc.utm2LatLon(tmp);
             latlon += String.format("%s %s, ", ll[1], ll[0]);
@@ -82,53 +80,77 @@ public class GpxCreator {
     }
 
 
-    public static void main( String[] args )
-            throws Exception
-    {
+    public static void main(String[] args)
+            throws Exception {
         /*  first, get and initialize an engine  */
         /*  next, get the Template  */
         String velocityTemplate = "WEB-INF/gpx_rte.vm";
         String inputSource = "Grundstuecke.csv";
 
-        if ( args.length > 0 ) {
+        if (args.length > 0) {
             velocityTemplate = args[0];
         }
-        if ( args.length > 1 ) {
+        if (args.length > 1) {
             inputSource = args[1];
         }
         String outputSource = null;
         GpxCreator gpxCreator = new GpxCreator();
-        if ( args.length > 2 ) {
+        if (args.length > 2) {
             outputSource = args[2];
         }
 
-        gpxCreator.createGpx(inputSource,outputSource);
+        gpxCreator.createGpx(inputSource, outputSource, OutputType.GPX_TRACK);
 
     }
 
-    public void createGpx ( String inputSource, String outputSource ) throws IOException {
-        String velocityTemplate = "gpx_rte.vm";
+    public enum OutputType {
+        GPX_ROUTE,
+        GPX_TRACK,
+        GPX_PLAIN,
+        KML_POLYGON
+    }
+    private static final List<String> templates = Arrays.asList("gpx_rte.vm","gpx_trk.vm","gpx_plain.vm","kml_polygon.vm");
+    public void createGpx(String inputSource, String outputSource, OutputType outputType ) {
+
+        String velocityTemplate = templates.get(outputType.ordinal());
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        ve.setProperty("classpath.resource.loader.class",ClasspathResourceLoader.class.getName());
-        ve.init();
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        FileWriter fileWriter = null;
 
-        Template t = ve.getTemplate( velocityTemplate );
+        try {
+            ve.init();
+            Template t = ve.getTemplate(velocityTemplate);
         /*  create a context and add data */
-        VelocityContext context = new VelocityContext();
-        context.put("header", System.getProperty("header") );
-        context.put("wfs", createGrundStuecke(inputSource));
+            VelocityContext context = new VelocityContext();
+            context.put("header", System.getProperty("header"));
+            context.put("wfs", createGrundStuecke(inputSource));
 
-        StringWriter writer = new StringWriter(100000);
-        t.merge( context, writer );
-        String content = writer.toString().replace("ä", "ae").replace("Ä", "Ae").replace("ü", "ue").replace("Ü", "Ue").replace("ö", "oe").replace("Ö", "Oe");
-        if ( outputSource == null ) {
-            System.out.println(content);
-        } else {
-            FileWriter fileWriter = new FileWriter(outputSource);
-            fileWriter.write(content);
+            StringWriter writer = new StringWriter(100000);
+            t.merge(context, writer);
+            String content = writer.toString()
+                    .replace("ä", "ae")
+                    .replace("Ä", "Ae")
+                    .replace("ü", "ue")
+                    .replace("Ü", "Ue")
+                    .replace("ö", "oe")
+                    .replace("Ö", "Oe")
+                    .replace("ß","ss");
+            if (outputSource == null) {
+                System.out.println(content);
+            } else {
+                fileWriter = new FileWriter(outputSource);
+                fileWriter.write(content);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("could not open file wiriter",e );
+        } finally {
+            if ( fileWriter != null ) try {
+                fileWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException("could not close file wiriter",e );
+            }
         }
-
     }
 
 }
