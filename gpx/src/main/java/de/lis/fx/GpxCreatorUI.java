4603d6cc29dcc694ede7bb0acfb58630a613edf3
letsch <detlef.letsch@web.de>
2016-09-25 11:30:11 +0200

    java fx extensions (incomplete)

--- gpx/src/main/java/de/lis/fx/GpxCreatorUI.java
+++ gpx/src/main/java/de/lis/fx/GpxCreatorUI.java
@@ -8,9 +8,14 @@ import de.lis.gpx.GpxCreator;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -22,7 +27,66 @@ import java.util.Optional;
 
 public class GpxCreatorUI extends Application {
 
-    @Override
+    public void startOff(final Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+
+        GridPane gridPane = new GridPane();
+        Scene sceene = new Scene(gridPane, 600, 550);
+
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        Button btnOutput = new Button();
+        RadioButton btnGpx = new RadioButton("GPX");
+        btnGpx.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                event.getSource();
+            }
+        });
+        btnGpx.setToggleGroup(toggleGroup);
+        btnGpx.setUserData(GpxCreator.OutputType.GPX_ROUTE);
+        gridPane.add(btnGpx, 1, 1);
+        RadioButton btnKml = new RadioButton("KML");
+        btnKml.setToggleGroup(toggleGroup);
+        btnKml.setUserData(GpxCreator.OutputType.KML_POLYGON);
+        gridPane.add(btnKml, 2, 1);
+
+        toggleGroup.selectToggle(btnGpx);
+
+        Label labelTitle = new Label("Titel");
+        gridPane.add(labelTitle, 1, 2);
+
+        TextField textFieldTitle = new TextField();
+        gridPane.add(textFieldTitle, 2, 2);
+
+        Button btnInput = new Button("CSV Input");
+        gridPane.add(btnInput, 1, 3);
+        TextField textFieldInput = new TextField();
+        textFieldInput.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                FileChooser inFileChooser = new FileChooser();
+                inFileChooser.setTitle("Grundstück Datei (.csv) auswählen.");
+                inFileChooser.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("CSV Files", "*.csv"),
+                        new FileChooser.ExtensionFilter("All Files", "*.*"));
+                File selectedFile = inFileChooser.showOpenDialog(primaryStage);
+                TextField orig = (TextField) event.getSource();
+                orig.setUserData(selectedFile);
+            }
+        });
+        gridPane.add(textFieldInput, 2, 3);
+
+
+        gridPane.add(btnOutput, 1, 4);
+        Button btnGenerate = new Button("Generiere GPX");
+        gridPane.add(btnGenerate, 1, 5);
+
+        primaryStage.setTitle("Titel");
+        primaryStage.setScene(sceene);
+
+        primaryStage.show();
+
+    }
+
     public void start(Stage primaryStage) throws Exception {
 /*
         final ToggleGroup group = new ToggleGroup();
@@ -76,14 +140,14 @@ public class GpxCreatorUI extends Application {
             selectedFile = outFileChooser.showSaveDialog(null);
             if (selectedFile != null) {
                 String header = System.getProperty("header");
-                if ( header == null || header.isEmpty() ) {
+                if (header == null || header.isEmpty()) {
                     header = "Generierte GPX Koordinaten";
                     TextInputDialog textInputDialog = new TextInputDialog();
                     textInputDialog.setTitle("Überschrift");
                     textInputDialog.setContentText("Titel");
                     textInputDialog.setHeaderText("Wähle einen Titel");
                     Optional<String> inputHeader = textInputDialog.showAndWait();
-                    if ( inputHeader.isPresent() ) {
+                    if (inputHeader.isPresent()) {
                         header = inputHeader.get();
                     }
                     System.setProperty("header", header);
